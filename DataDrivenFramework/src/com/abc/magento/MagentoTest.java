157eb79da5e3876b9c156754b87f75869c20b800
Priyanka <Priyanka@DESKTOP-5EL6MDO>
2019-07-10 11:53:10 +0530

    commit one

---
+++ DataDrivenFramework/src/com/abc/magento/MagentoTest.java
@@ -0,0 +1,52 @@
+package com.abc.magento;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MagentoTest {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fis = new FileInputStream
+		   ("C:\\Users\\HP ENVY\\Desktop\\March\\DataDrivenFramework\\src\\com\\abc\\utilities\\DataDriven.properties");
+        Properties p = new Properties();
+        p.load(fis);
+        
+        String nurl = p.getProperty("url");
+        String nemail = p.getProperty("email");
+        String npass = p.getProperty("pass");
+        
+        System.out.println(nurl);
+        System.out.println(nemail);
+        System.out.println(npass);
+        
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait
+                   (20, TimeUnit.SECONDS);
+        driver.get(nurl);
+        
+        driver.findElement(By.linkText("My Account"))
+                           .click();
+        driver.findElement(By.id("email"))
+                       .sendKeys(nemail);
+        driver.findElement(By.id("pass"))
+                       .sendKeys(npass);
+        driver.findElement(By.id("send2"))
+                    .click();
+        driver.findElement(By.linkText("Log Out"))
+        .click();
+        driver.close();
+        
+        
+        
+        
+	}
+
+}
